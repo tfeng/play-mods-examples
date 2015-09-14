fae899b1b243814a8d726b56eab3eceb1625e668
Thomas Feng <tfeng@linkedin.com>
2015-09-14 12:14:59 -0700

    Display a success message after each test.

--- run-all.sh
+++ run-all.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 for dir in *-example; do
-  if ! (cd $dir; activator $@); then
+  if (cd $dir; activator $@); then
+    echo "Test(s) succeeded in $dir"
+  else
     echo "Test(s) failed in $dir"
     exit 1
   fi
